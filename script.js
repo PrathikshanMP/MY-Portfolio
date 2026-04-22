82acf8fea4bbd5cbc30b7a1428bb0f560560795d
PrathikshanMP <shangamer.com@gmail.com>
2026-04-23 00:17:10 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,47 @@
+var tablinks = document.getElementsByClassName("tab-links");
+var tabcontents = document.getElementsByClassName("tab-contents");
+
+
+function opentab(tabname) {
+    for (tablink of tablinks) {
+        tablink.classList.remove("active-link");
+    }
+    for (tabcontent of tabcontents) {
+        tabcontent.classList.remove("active-tab");
+    }
+    event.currentTarget.classList.add("active-link");
+    document.getElementById(tabname).classList.add("active-tab");
+}
+var sidemenu = document.getElementById("sidemenu");
+
+function openmenu() {
+    sidemenu.style.right = "0";
+}
+function closemenu() {
+    sidemenu.style.right = "-200px";
+}
+
+var menuLinks = document.querySelectorAll("#sidemenu li a");
+
+menuLinks.forEach(function (link) {
+    link.onclick = function () {
+        closemenu();
+    };
+});
+//sucuss notification
+const contactForm = document.getElementById("contact-form");
+
+contactForm.addEventListener('submit',function(e) {
+    e.preventDefault();
+
+    alert("Success! Your message has been sent successfully.");
+    window.location.reload();
+});
+//side menu styling
+function openmenu() {
+    sidemenu.style.right = "20px"; 
+}
+
+function closemenu() {
+    sidemenu.style.right = "-250px";
+}
